1a9fe0df55fa1cf2778d4f069a64e4ebd2e2fdb6
Charlie Skibsted <charlieskibsted@gmail.com>
2023-04-26 16:36:51 +0200

    Latest edition Added so the user can view the real time score if needed.

--- Labb 3 – Trådar och asynkront/Car.cs
+++ Labb 3 – Trådar och asynkront/Car.cs
@@ -13,13 +13,17 @@ namespace Labb_3___Trådar_och_asynkront
         public string Name { get; set; }
         public int Speed { get; set; }
         public double DistanceInKm { get; set; }
+        public static string Winner;
+
+        public TimeSpan RaceTime { get; set; }
 
         public Car(string name, int speed, int distanceInKm)
         {
             Name = name;
             Speed = speed;
             DistanceInKm = distanceInKm;
-            
+
+
         }
 
         public void StartRace()
@@ -27,25 +31,90 @@ namespace Labb_3___Trådar_och_asynkront
             Stopwatch sw = Stopwatch.StartNew();
             sw.Start();
 
-            int i;
+            Console.WriteLine($"{Name} started");
 
-            for (i = 0; i <= 300; i++) // 1i = seconds
+            for (int i = 0; i <= 300; i++) // 1i = seconds
             {
                 Thread.Sleep(1000); //Every iteration is 1 second long
 
                 DistanceInKm += (double)Speed / 3600; // //Divide by 3600 to get km/seconds
 
-                Console.WriteLine($"{Name} distance in kilometers: {DistanceInKm}");
-
                 if (DistanceInKm >= 1)
                 {
                     break;
                 }
+
+
+                if (i == 5 || i == 10 || i == 15 || i == 20 || i == 25 || i == 30 || i == 35 || i == 40 || i == 45 || i == 50 || i == 55 || i == 60)
+                {
+                    CarProblem();
+                }
             }
+            Console.WriteLine($"{Name} has finished the race");
+
+            if (Car.Winner == null)
+            {
+                Car.Winner = Name;
+                Console.WriteLine($"Car:{Name} is declared the Winner. Congratz!!!");
+            }
+            RaceTime = sw.Elapsed;
+            sw.Stop();
 
             
-            Console.WriteLine($"{Name} has finished the race in {sw.Elapsed} milli seconds!");
-            sw.Stop();
+        }
+
+        
+
+        public void CarProblem()
+        {
+
+            Random random = new Random();
+
+            int randomIncident = random.Next(1, 101);
+
+
+            ///ODDSEN ÄR FEL, FIXA SEN
+            if (randomIncident > 0 && randomIncident <= 2)
+            {
+                IsOutOfGas();
+            }
+            else if (randomIncident > 2 && randomIncident <= 6)
+            {
+                HasFlatTire();
+            }
+            else if (randomIncident > 6 && randomIncident <= 16)
+            {
+                HasBirdOnWindShield();
+            }
+            else if(randomIncident > 16 && randomIncident <= 36)
+            {
+                HasEngineFailure();
+            }
+
+        }
+
+        public void IsOutOfGas()
+        {
+            Console.WriteLine($"{Name} is out of gas, need 3 seconds to fill the gastank.");
+            Thread.Sleep(3000);
+        }
+
+        public void HasFlatTire()
+        {
+            Console.WriteLine($"{Name} has a flat tire, it takes 2 seconds to fix it.");
+            Thread.Sleep(2000);
+        }
+
+        public void HasBirdOnWindShield()
+        {
+            Console.WriteLine($"{Name} has a bird on the windshield, it takes 1 seconds to fix it.");
+            Thread.Sleep(1000);
+        }
+
+        public void HasEngineFailure()
+        {
+            Speed = Speed - 1;
+            Console.WriteLine($"{Name} has engine failure, the speed is reduced by 1km/h, new {Speed} km/h");
         }
 
     }
--- Labb 3 – Trådar och asynkront/Program.cs
+++ Labb 3 – Trådar och asynkront/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace Labb_3___Trådar_och_asynkront
 {
@@ -9,11 +10,38 @@ namespace Labb_3___Trådar_och_asynkront
             Car car1 = new Car("Car 1", 100, 0);
             Car car2 = new Car("Car 2", 100, 0);
 
+
             Thread thread1 = new Thread(new ThreadStart(car1.StartRace));
             Thread thread2 = new Thread(new ThreadStart(car2.StartRace));
 
+            Thread thread3 = new Thread(new ThreadStart(() => PrintCarInfo(car1, car2)));
+
+            Console.WriteLine("Press any key to view the the scores of the race");
+
             thread1.Start();
             thread2.Start();
+            thread3.Start();
+
+            
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("Scores: ");
+            Console.WriteLine($"Car: {car1.Name} Time: {car1.RaceTime} ");
+            Console.WriteLine($"Car: {car2.Name} Time: {car2.RaceTime} ");
+
+        }
+
+        public static void PrintCarInfo(Car car1, Car car2)
+        {
+            while (Car.Winner == null)
+            {
+                Console.ReadKey();
+                Console.WriteLine($"{car1.Name} distance in kilometers: {car1.DistanceInKm} speed: {car1.Speed}");
+                Console.WriteLine($"{car2.Name} distance in kilometers: {car2.DistanceInKm} speed: {car2.Speed}");
+            }
+
         }
 
     }
