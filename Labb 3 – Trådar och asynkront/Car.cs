d44ad49653e8568c9be86385dca407393c906541
Charlie Skibsted <charlieskibsted@gmail.com>
2023-04-26 11:58:13 +0200

    Add project files.

---
+++ Labb 3 – Trådar och asynkront/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Labb_3___Trådar_och_asynkront
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public double DistanceInKm { get; set; }
+
+        public Car(string name, int speed, int distanceInKm)
+        {
+            Name = name;
+            Speed = speed;
+            DistanceInKm = distanceInKm;
+            
+        }
+
+        public void StartRace()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            sw.Start();
+
+            int i;
+
+            for (i = 0; i <= 300; i++) // 1i = seconds
+            {
+                Thread.Sleep(1000); //Every iteration is 1 second long
+
+                DistanceInKm += (double)Speed / 3600; // //Divide by 3600 to get km/seconds
+
+                Console.WriteLine($"{Name} distance in kilometers: {DistanceInKm}");
+
+                if (DistanceInKm >= 1)
+                {
+                    break;
+                }
+            }
+
+            
+            Console.WriteLine($"{Name} has finished the race in {sw.Elapsed} milli seconds!");
+            sw.Stop();
+        }
+
+    }
+
+}
---
+++ Labb 3 – Trådar och asynkront/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace Labb_3___Trådar_och_asynkront
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car car1 = new Car("Car 1", 100, 0);
+            Car car2 = new Car("Car 2", 100, 0);
+
+            Thread thread1 = new Thread(new ThreadStart(car1.StartRace));
+            Thread thread2 = new Thread(new ThreadStart(car2.StartRace));
+
+            thread1.Start();
+            thread2.Start();
+        }
+
+    }
+}
